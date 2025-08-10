a06d265d937755503bc177fc4e17807ed6540c53
joebaga <jlirenge@gmail.com>
2025-08-10 06:56:09 +0300

    size changed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,9 +11,22 @@ export default function Home() {
         backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.55), rgba(0, 0, 0, 0.55)), url(${bgImage})`
       }}
     >
-      <h1> Coming soon </h1>
+      <h1> Korean Touch, Global Reach </h1>
+      <p>
+        coming soon...
+      </p>
       <p>
         contact us
+        
+      </p>
+      <p>
+        
+        📩 Email: info@koarex.com
+      </p>
+
+      <p>
+        
+        📞 WhatsApp: +82 10 2488 0555
       </p>
       <button onClick={() => (window.location.href = "/about")}>
         Discover KOAREX
